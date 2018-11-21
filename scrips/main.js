45433ef297cd0041fbba36b67841afd6a4cbbdac
Yong Dong <yongdong11467@gmail.com>
2018-11-21 15:35:35 -0500

    improved events?

--- scrips/main.js
+++ scrips/main.js
@@ -1,11 +1,28 @@
 var search = document.getElementsByClassName("form-control mr-sm-2")[0];
+var inputWebName = document.getElementById("nameValInput");
+var displayWebNameHint = document.getElementById("nameVal");
 
 function welcome() {
     alert("Welcome to Yong Kai\'s web")
 }
-search.addEventListener("click", function(){
+
+
+function validateWebName() {
+    var input = inputWebName.value;
+    if (input == "Personalized") {
+        displayWebNameHint.innerHTML = "BottomLeft Click"
+    } else {
+        displayWebNameHint.innerHTML = input + "?"
+    }  
+    //alert(input)
+}
+
+inputWebName.addEventListener("input", validateWebName)
+
+search.addEventListener("input", function(){
     alert("This part is not done!");
 })
+
 
 
 
